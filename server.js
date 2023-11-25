999ae27d2b3336e73eecd10cee5411ececea5834
udaysaimanoj123 <117463621+udaysaimanoj123@users.noreply.github.com>
2023-11-25 14:50:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use("/api/v1/product",productRoutes );
 //rest api
 app.use("*",function(req,res){
   res.sendFile(path.join(__dirname,"./client/build/index.html"));
-}
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
