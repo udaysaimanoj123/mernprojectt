71a38d367c4d9db8c3af2405de10511a1d66adf2
udaysaimanoj123 <117463621+udaysaimanoj123@users.noreply.github.com>
2023-11-25 14:42:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use("/api/v1/product",productRoutes );
 
 
 //rest api
-app.use("*",function(req,res)){
+app.use("*",function(req,res){
   res.sendFile(path.join(__dirname,"./client/build/index.html"));
 }
 
